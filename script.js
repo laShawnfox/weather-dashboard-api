9cc05a548ed35bed98f0c76ef03598aa65ab3a82
Shakana Fox <foxshakana@gmail.com>
2020-10-09 13:19:02 -0400

    implented weather api and jumbotron for city weather and changed search button color

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    console.log("hello world");
+    //console.log("hello world");
+   $.ajax({
+       url: 
+       "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=570e7463f975999cf525695cc31f504d",
+       
+       method: "GET",
+   });
+   //console.log("api");
+
+
 })
