7a919736497eb45bddd6966ffb7980a15f55b889
Shakana Fox <foxshakana@gmail.com>
2020-10-09 14:51:34 -0400

    added city buttons for html

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 $(document).ready(function(){
     //console.log("hello world");
-   $.ajax({
-       url: 
-       "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=570e7463f975999cf525695cc31f504d",
+    //dom VARAIABLES
+  
+    var searchBtn = document.getElementById("searchBtn");
+    var jumbotron = document.getElementsByClassName("jumbotron");
+    var cityBtns   = document.getElementById("cityBtns");
+    //JS VARIABLES
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=570e7463f975999cf525695cc31f504d";
+    
+
+   
+
+    //FUNCTIONS
+    //WHEN I search for a city and click button
+    $("#searchBtn").on("click", function (){
+
+        $.ajax({
+            url: 
+            "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=570e7463f975999cf525695cc31f504d",
+            
+            method: "GET",
+        });
+    })
+       //buttons of cities when clicked should populate to the right of screen
+       //THEN I am presented with the city name, the date,...in top right jumbotron
+
        
-       method: "GET",
-   });
-   //console.log("api");
+       // THEN I am presented with current and future conditions for that city and that city is added to the search history
+   
+
+       
+
+
+   
 
 
 })
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
