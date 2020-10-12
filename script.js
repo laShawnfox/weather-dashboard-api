d56ceb622f7253fc6f277e66185ff32beb7befa3
Shakana Fox <foxshakana@gmail.com>
2020-10-11 20:52:38 -0400

    changed jumbotron color

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-console.log("hello world");
+//console.log("hello world");
+function getWeather(event){
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=570e7463f975999cf525695cc31f504d",
+    method: "GET",
+}).then(function(response){
+    console.log(response.city);
 });
+}
+
+$("#search-form").on("submit", function(){
+    event.preventDefault();
+    getWeather();
+})
+
+});
+//api.openweathermap.org/data/2.5/forecast?q={city name}
