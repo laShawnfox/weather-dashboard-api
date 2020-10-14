193ed249a3020b8585a9db65cc5a38d3c0b1ff44
Shakana Fox <foxshakana@gmail.com>
2020-10-14 13:53:53 -0400

    changd search form

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
 //console.log("hello world");
+var fiveDay = $("#fiveDay");
+
+
 function getWeather(searchCity) {
+    console.log(searchCity)
+    var searchCity = $("#user-input").val();
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=570e7463f975999cf525695cc31f504d";
     var apiKey = "570e7463f975999cf525695cc31f504d";
 $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey,
+    url: queryUrl, 
     method: "GET",
 }).then(function(response){
-    console.log(response.list);
+    console.log(response);
+ fiveDay.html("");
+
+    for (var i = 0; i < response.list.length; i + 8){
+    fiveDay.append("<div>");
+        var weather = $("<h3>");
+        weather.text(response.list[i].main.temp);
+        $("#fiveDay").append(weather);   
+    }
 });
 }
+ 
 
  $("#searchBtn").on("click", function(event){
     event.preventDefault();
-    var searchCity = $("#user-input").val();
+    searchCity = $("#user-input").val();
     console.log(searchCity);
      getWeather(searchCity);
+     //getCities();
+     //want 5 day forecast to populate 
  })
 
 });
