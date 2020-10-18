b1bc00082a2c57c2eda8c5736b5ac2c3b1f8ec68
Shakana Fox <foxshakana@gmail.com>
2020-10-17 23:17:28 -0400

    created query for current weather

--- script.js
+++ script.js
@@ -7,43 +7,65 @@ var currentHumidity = $("#humidity");
 var currentWind = $("#wind-speed");
 var uv = $("#uv");
 var searchHistory = JSON.parse(localStorage.getItem("search-form"));
-var cityName = $("#user-input");
+var city = $("#user-input");
 var cityDisplay = $("#city-display");
 var currentDay = moment().format("HH");
-var UV = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=463f975999cf525570e7695cc31f504d";
-
+var UV = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=463f975999cf525570e7695cc31f504d";
+// var lat = response.city.coord.lat;
+// var lon = response.city.coord.lon;
 
 // Function search city
-function getWeather(cityName) {
-   console.log(cityName);
+function getWeather(city) {
+   //console.log(cityName);
     var apiKey = "463f975999cf525570e7695cc31f504d";
-    var cityName = $("#user-input").val();
+    var city = $("#user-input").val();
    
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
    
 $.ajax({
     url: queryURL, 
     method: "GET",
 }).then(function(response){
     console.log(response);
- //fiveDay.html("");
+   console.log(response.name);
+   fiveDay.html("");
 
-    // for (var i = 0; i < response.length; i + 8){
-    // fiveDay.append("<div>");
-    //     var weather = $("<h3>");
-    //     weather.text(response.list.main[0].temp);
-    //     $("#fiveDay").append(weather);   
-    // }
+ //Five Day Forecast
+     for (var i = 0; i < response.length; i + 8){
+     fiveDay.append("<div>");
+        var weather = $("<h3>");
+        weather.text(response.list.main[0].temp);
+        $("#fiveDay").append(weather);   
+     }
 
 });
 }
  
+//Current weather
+var queryURLcurrent = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+var apiKey = "463f975999cf525570e7695cc31f504d";
 
- $("#searchBtn").on("click", function(event){
+$.ajax({
+    url: queryURLcurrent, 
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+
+
+         
+
+$("#display-name").text(response.name);
+var icon =response.weather[0].icon;
+$("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
+
+ $("#searchBtn").on("click", function(){
+   getWeather(city);
+    city = $("#user-input").val();
+    console.log(city);
+    console.log(cityDisplay);
+    localStorage.getItem(city);
+    //currentWeather(city);
    
-   // cityName = $("#user-input").val();
-    console.log(cityName);
-    // getWeather(cityName);
      //localStorage.setItem("search", JSON.stringify("search-form"));
      //getCities();
      
