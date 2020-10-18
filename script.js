b496c056c4d24b2aaa92096eefa4c8e44c0bd685
Shakana Fox <foxshakana@gmail.com>
2020-10-17 22:41:40 -0400

    added id to cards and changed varaiable names

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ var currentHumidity = $("#humidity");
 var currentWind = $("#wind-speed");
 var uv = $("#uv");
 var searchHistory = JSON.parse(localStorage.getItem("search-form"));
-var searchCity = $("#user-input");
-var cityName = $("#city-display");
+var cityName = $("#user-input");
+var cityDisplay = $("#city-display");
+var currentDay = moment().format("HH");
 var UV = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=463f975999cf525570e7695cc31f504d";
 
+
+// Function search city
 function getWeather(cityName) {
-   // console.log(cityName);
+   console.log(cityName);
     var apiKey = "463f975999cf525570e7695cc31f504d";
-    var searchCity = $("#user-input").val();
+    var cityName = $("#user-input").val();
+   
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
    
 $.ajax({
@@ -22,28 +26,30 @@ $.ajax({
     method: "GET",
 }).then(function(response){
     console.log(response);
- fiveDay.html("");
+ //fiveDay.html("");
 
-    for (var i = 0; i < response.length; i + 8){
-    fiveDay.append("<div>");
-        var weather = $("<h3>");
-        weather.text(response.list.main[0].temp);
-        $("#fiveDay").append(weather);   
-    }
+    // for (var i = 0; i < response.length; i + 8){
+    // fiveDay.append("<div>");
+    //     var weather = $("<h3>");
+    //     weather.text(response.list.main[0].temp);
+    //     $("#fiveDay").append(weather);   
+    // }
 
 });
 }
  
 
  $("#searchBtn").on("click", function(event){
-    event.preventDefault();
-    searchCity = $("#user-input").val();
-    //console.log(searchCity);
-     getWeather(searchCity);
+   
+   // cityName = $("#user-input").val();
+    console.log(cityName);
+    // getWeather(cityName);
      //localStorage.setItem("search", JSON.stringify("search-form"));
      //getCities();
      
  })
+
+
 
 });
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key
